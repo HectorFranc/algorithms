23c503f3137559545b15807ea07bd53caafef318
Hector Franco Torres <tor6.franco6@hotmail.com>
2019-12-30 15:18:55 -0600

    bubbleSort implementation

--- sortAlgorithms/bubbleSort.py
+++ sortAlgorithms/bubbleSort.py
@@ -2,3 +2,13 @@ def _swap(lst, indexA, indexB):
     temp = lst[indexA]
     lst[indexA] = lst[indexB]
     lst[indexB] = temp
+
+
+def own_sort(lst):
+    actual_max_index = len(lst) - 1
+    while actual_max_index > 0:
+        for i in range(0, actual_max_index):
+            if lst[i] > lst[i + 1]:
+                _swap(lst, i, i + 1)
+        actual_max_index -= 1
+    return lst
