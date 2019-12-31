7a06cae102c602a18a250434cb97fac8cf997fff
Hector Franco Torres <tor6.franco6@hotmail.com>
2019-12-30 20:09:49 -0600

    Docstring for factorial function

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -1,4 +1,10 @@
 def factorial(n):
+    '''
+    Returns n! (n factorial)
+    Parameters:
+    n: number non-negative
+        Number that will be used
+    '''
     n = int(n)
     if n < 0:
         raise RecursionError('n is < 0. n should be >= 0')
