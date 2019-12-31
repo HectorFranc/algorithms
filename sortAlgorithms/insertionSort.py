77431563d695382751519a96bf506c8c664e39b1
Hector Franco Torres <tor6.franco6@hotmail.com>
2019-12-30 19:22:21 -0600

    insertion_sort function

--- sortAlgorithms/insertionSort.py
+++ sortAlgorithms/insertionSort.py
@@ -2,3 +2,13 @@ def _swap(lst, indexA, indexB):
     temp = lst[indexA]
     lst[indexA] = lst[indexB]
     lst[indexB] = temp
+
+
+def insertion_sort(lst):
+    for i in range(1, len(lst)):
+        tmp = lst[i]
+        actual_index = i - 1
+        while str(tmp) < str(lst[actual_index]) and actual_index >= 0:
+            lst[actual_index + 1] = lst[actual_index]
+            actual_index -= 1
+        lst[actual_index + 1] = tmp
