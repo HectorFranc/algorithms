48bf268b3ed60cd976031c668287907d0264de35
Hector Franco Torres <tor6.franco6@hotmail.com>
2019-12-31 16:43:25 -0600

    quick sort implementation

--- divideAndConquer/quickSort.py
+++ divideAndConquer/quickSort.py
@@ -1,2 +1,27 @@
 def swap(lst, indexA, indexB):
     lst[indexA], lst[indexB] = lst[indexB], lst[indexA]
+
+
+def quickSort(lst, min_inclusive=0, max_inclusive=None):
+    if max_inclusive is None:
+        # Max inclusive will be the pivot
+        max_inclusive = len(lst) - 1
+
+    # If not in base case: list has not 0 or 1 element (are not already ordered)
+    if min_inclusive < max_inclusive:
+
+        # Group separator
+        index_min_of_group_bigger = min_inclusive
+        # Partition
+        pivot_value = lst[max_inclusive]
+
+        for index_min_of_group_unknown in range(min_inclusive, max_inclusive):
+            if lst[index_min_of_group_unknown] <= pivot_value:
+                swap(lst, index_min_of_group_bigger, index_min_of_group_unknown)
+                index_min_of_group_bigger += 1
+
+        swap(lst, index_min_of_group_bigger, max_inclusive)
+
+        # Recursion
+        quickSort(lst, min_inclusive, index_min_of_group_bigger - 1)
+        quickSort(lst, index_min_of_group_bigger + 1, max_inclusive)
