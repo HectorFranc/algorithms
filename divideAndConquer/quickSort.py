88f0fe1b9efcc69a8101eb1bb12ca42016ef460c
Hector Franco Torres <tor6.franco6@hotmail.com>
2019-12-31 16:53:50 -0600

    Docstrings for quickSort

--- divideAndConquer/quickSort.py
+++ divideAndConquer/quickSort.py
@@ -3,6 +3,23 @@ def swap(lst, indexA, indexB):
 
 
 def quickSort(lst, min_inclusive=0, max_inclusive=None):
+    '''
+    Quick sort algorithm.
+
+    Parameters:
+
+    - lst: list
+
+        List to be sorted.
+
+    - min_inclusive: int, optional, default: 0
+
+        Minimum index of sub-list to be sorted.
+
+    - max_inclusive: int, optional, default: len(lst) - 1
+
+        Maximum index of sub-list to be sorted.
+    '''
     if max_inclusive is None:
         # Max inclusive will be the pivot
         max_inclusive = len(lst) - 1
